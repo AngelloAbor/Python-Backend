608928dae7093b5bcc375e768d58d5e0050b4429
angello orrego <angello.bor@gmail.com>
2025-02-19 10:54:41 -0300

    bd nuve
    bd nuve 1.0

--- FastApi/db/client.py
+++ FastApi/db/client.py
@@ -1,3 +1,6 @@
 from pymongo import MongoClient
 
 db_client = MongoClient()
+
+#remota
+#db_client = MongoClient("mongodb+srv://angellobor:Abor1841.@cluster0.pwevm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
